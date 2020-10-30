48f30307f171425d56a9015abac62fafa1ae6cc6
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 15:15:58 +0430

    Add imute file with some concepts

---
+++ imute.py
@@ -0,0 +1,9 @@
+fruits = ['Apple', 'Banana', 'Orange']
+
+other_fruits = fruits.copy()
+
+other_fruits.append('Oregano')
+
+print(fruits, other_fruits)
+
+#Lists, Sets, dict -> ALWAYS KEEP REFERENCE
