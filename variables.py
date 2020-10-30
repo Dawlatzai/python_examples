e8dfac6f77c38d74ac976b860cffac5bf91ec6d6
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 14:16:57 +0430

    print out the variables value

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 cart = 3 #this is a number
 name = 'Dawlatzai' #String
 currency = 4.3 #Float
-loggen_in = true #Boolean
-print(cart)
+logged_in = True #Boolean
+print(cart, name, currency, logged_in)
