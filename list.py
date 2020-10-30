92b68a9d27f5ef4d9fa6eb0f720483262fe5318c
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 15:00:48 +0430

    Add list file with its some predefined methods

---
+++ list.py
@@ -0,0 +1,30 @@
+users = ['ALi', 'Khan', 'Jahan', 'Kalan', 'Kalam', 'KhanJan', 'Adam']
+
+print(users[2])
+
+print(users[:4])
+
+print(users[4:])
+
+#Unpacking
+
+items = ['Laptop', 'Phone', 'Joystick']
+
+print(items[0])
+
+laptop, phone, joystick = items
+
+print(laptop)
+
+laptop , *other = items
+
+print(other)
+
+#Add to array items.append()
+#Insert to array items.insert(0, value)
+#out from an array items.pop()
+
+#Tupples
+letter = ('a', 'b', 'b', 'c', 'd')
+#Sets , have unique value 
+letters = {'a', 'b', 'b', 'c', 'd'}
