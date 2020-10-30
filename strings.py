f4cf90d71b7dbd0dbd26c8f926b0a7d3ba0707e0
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 14:34:54 +0430

    add string file with some predefined methods

--- strings.py
+++ strings.py
@@ -9,3 +9,14 @@ print(language[0:3])
 print(language[:4])
 
 print(language[-1])
+
+print(language.upper())
+
+print(language.lower())
+
+print(language.replace('P','test'))
+
+print(language.find('t'))
+
+
+
