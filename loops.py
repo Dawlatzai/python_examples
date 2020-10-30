07aee4dcb373698b417bbe5a2c4233e37a0f9c85
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 15:08:33 +0430

    Add loops

---
+++ loops.py
@@ -0,0 +1,17 @@
+users = ['ALi', 'Khan', 'Jahan', 'Kalan', 'Kalam', 'KhanJan', 'Adam']
+
+for name in users:
+  print(name)
+
+list_range = list(range(0,10))
+
+print(list_range)
+
+list_range1 = list(range(0,101, 5))
+
+print(list_range1)
+
+name = 'Dawlatzai'
+
+for letter in name:
+  print(letter)
