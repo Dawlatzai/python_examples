21dd5386cb7987beb3efb3e49f235aca3e347678
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-10-30 15:22:27 +0430

    Add function file with some example

---
+++ functions.py
@@ -0,0 +1,13 @@
+def greeting():
+    print('Hello nice to meet you')
+    print('What is your name?')
+    name = input()
+
+    print('Nice to meet you: '+ name)
+
+greeting()
+
+def sleeping(t):
+    print('Excuse me, I am sleeping: '+ t)
+
+sleeping('Khan')
